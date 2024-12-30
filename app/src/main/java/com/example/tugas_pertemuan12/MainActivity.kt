3ef2295eeab925947d633e1f88e30531a2638e35
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-30 13:24:11 +0700

    Memanggil Mahasiswa App di main

---
+++ app/src/main/java/com/example/tugas_pertemuan12/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tugas_pertemuan12
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.tugas_pertemuan12.ui.theme.Tugas_Pertemuan12Theme
+import com.example.tugas_pertemuan12.ui.viewmodel.MahasiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tugas_Pertemuan12Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MahasiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
